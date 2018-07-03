42819a68547be0d9a3e2e0525dc2e72d94dfaa77
Elyas Nurgat <elyas.nurgat@autodesk.com>
2018-07-03 09:48:04 +0100

    koans 2.8: 9 out of 9

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -32,7 +32,7 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(productsICanEat.length).toBe(FILL_ME_IN);
+    expect(productsICanEat.length).toBe(1);
   });
 
   it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
@@ -40,8 +40,10 @@ describe("About Applying What We Have Learnt", function() {
       var productsICanEat = [];
 
       /* solve using filter() & all() / any() */
+      productsICanEat = _(products).filter(function (x) { return (x.containsNuts === false && x.ingredients.indexOf("mushrooms") === -1) });
 
-      expect(productsICanEat.length).toBe(FILL_ME_IN);
+
+      expect(productsICanEat.length).toBe(1);
   });
 
   /*********************************************************************************/
@@ -55,14 +57,21 @@ describe("About Applying What We Have Learnt", function() {
       }
     }
 
-    expect(sum).toBe(FILL_ME_IN);
+    expect(sum).toBe(233168);
   });
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
 
-    var sum = FILL_ME_IN;    /* try chaining range() and reduce() */
+    var myarray = _.range(1,1000,1);
+    var result = _(myarray).chain()
+                       .filter(function(x) { return (x % 3 === 0 || x % 5 == 0) } )
+                       .reduce(function (sum, x) { return sum + x })
+                       .value();
 
-    expect(233168).toBe(FILL_ME_IN);
+    /* var sum = s; */    /* try chaining range() and reduce() */
+
+
+    expect(233168).toBe(result);
   });
 
   /*********************************************************************************/
@@ -75,15 +84,28 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(ingredientCount['mushrooms']).toBe(FILL_ME_IN);
+    expect(ingredientCount['mushrooms']).toBe(2);
   });
 
   it("should count the ingredient occurrence (functional)", function () {
     var ingredientCount = { "{ingredient name}": 0 };
 
     /* chain() together map(), flatten() and reduce() */
-
-    expect(ingredientCount['mushrooms']).toBe(FILL_ME_IN);
+    ingredientCounts = _(products).chain()
+                            .map(function(item, index, arr) {return item.ingredients;})
+                            .flatten()
+  /*                          
+                            .reduce(function(sum, citem) {
+                                sum[citem] = sum[citem] + 1;
+                              return sum;
+                            }, ingredientCount)
+  */
+                            .value();
+    /* Not sure why reduce is not working. Work around: Convert Object to an Array and then use filter*/
+    var res = Object.keys(ingredientCounts).map(function(k) { return [+k, ingredientCounts[k]]; });
+    var ingredientmushroom = _(res).filter(function(x) { return x[1] === 'mushrooms'; })
+/*    expect(ingredientCount['mushrooms']).toBe(ingredientmushroom.length);*/
+    expect(ingredientmushroom.length).toBe(2);
   });
 
   /*********************************************************************************/
